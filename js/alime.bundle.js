687c25ce965a54a795bdf8fb1848f76cfda1d2f9
Yoon MiYeon <auseyoo@gmail.com>
2020-01-12 23:04:09 +0900

    포폴 깨짐 수정 제발 잘보이길..

--- js/alime.bundle.js
+++ js/alime.bundle.js
@@ -1650,7 +1650,7 @@ function(a, b, c, d) {
     };
     a.magnificPopup.registerModule("image", {
         options: {
-            markup: '<div class="mfp-figure"><div class="mfp-close"></div><figure><div class="mfp-img"></div><figcaption><div class="mfp-bottom-bar"><div class="mfp-title">테스트</div><div class="mfp-counter"></div></div></figcaption></figure></div>',
+            markup: '<div class="mfp-figure"><div class="mfp-close"></div><figure><div class="mfp-img"></div><figcaption><div class="mfp-bottom-bar"><div class="mfp-title"></div><div class="mfp-counter"></div></div></figcaption></figure></div>',
             cursor: "mfp-zoom-out-cur",
             titleSrc: "title",
             verticalFit: !0,
